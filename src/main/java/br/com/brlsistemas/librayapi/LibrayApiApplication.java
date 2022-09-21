0994087beb94b9f2e8fe7530009b9477f9bc1412
Anderson Oliveira <2175235+anderson-sfoliveira@users.noreply.github.com>
2022-09-21 18:29:42 -0300

    Update LibrayApiApplication.java

--- src/main/java/br/com/brlsistemas/librayapi/LibrayApiApplication.java
+++ src/main/java/br/com/brlsistemas/librayapi/LibrayApiApplication.java
@@ -4,13 +4,21 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 @SpringBootApplication
+@EnableScheduling
 public class LibrayApiApplication {
 
 	@Bean
 	public ModelMapper modelMapper() {
 		return new ModelMapper();
+	}
+
+	@Scheduled(cron = "0 0 5 * * *") // agendado para todo dia 5 da manhã
+	public void testeAgendamentoTarefas(){
+
 	}
 
 	public static void main(String[] args) {

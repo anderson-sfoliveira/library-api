5fa92faf46a794a6329b7bd97cc2658736188853
Dani Ambrósio <anderson.sfoliveira@gmail.com>
2022-09-22 11:53:20 -0300

    90. Documentando a API com Swagger

---
+++ src/main/java/br/com/brlsistemas/librayapi/config/SwaggerConfig.java
@@ -0,0 +1,44 @@
+package br.com.brlsistemas.librayapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@Configuration
+public class SwaggerConfig {
+
+    // http://localhost:8080/swagger-ui.html
+
+    @Bean
+    public Docket docket(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis( RequestHandlerSelectors.basePackage("br.com.brlsistemas.librayapi.api.resource") )
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo(){
+        return new ApiInfoBuilder()
+                .title("Library API")
+                .description("API do projeto de bibliotecas")
+                .version("1.0")
+                .contact(contact())
+                .build();
+    }
+
+    private Contact contact(){
+        return new Contact("Anderson Oliveira",
+                "https://github.com/anderson-sfoliveira",
+                "anderson.sfoliveira@gmail.com");
+    }
+}

5fb39e6a4f08eb45618332575e2385c6a84854b9
Dani Ambrósio <anderson.sfoliveira@gmail.com>
2022-09-21 23:23:16 -0300

    86. Testando o envio de emails

--- src/main/java/br/com/brlsistemas/librayapi/LibrayApiApplication.java
+++ src/main/java/br/com/brlsistemas/librayapi/LibrayApiApplication.java
@@ -1,5 +1,6 @@
 package br.com.brlsistemas.librayapi;
 
+import br.com.brlsistemas.librayapi.model.entity.Loan;
 import br.com.brlsistemas.librayapi.service.EmailService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,30 +9,41 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 @EnableScheduling
 public class LibrayApiApplication {
 
-	@Autowired
-	private EmailService emailService;
+//	@Autowired
+//	private EmailService emailService;
 
 	@Bean
 	public ModelMapper modelMapper() {
 		return new ModelMapper();
 	}
 
-	@Bean
-	public CommandLineRunner commandLineRunner(){
-		return args -> {
-			List<String> emails = Arrays.asList("91178ce32b-0d1201@inbox.mailtrap.io");
-			emailService.sendEmails("Testando serviço de e-mails.", emails);
-			System.out.println("Email enviado!");
-		};
-	}
+//	@Bean // métodos do tipo CommandLineRunner são executados assim o projeto é executado
+//	public CommandLineRunner commandLineRunner(){
+//		return args -> {
+//			sendMailTest();
+//		};
+//	}
+//
+//	@Scheduled(cron = "0 20 23 1/1 * *") // http://www.cronmaker.com/
+//	public void mailTestSchedule() {
+//		sendMailTest();
+//	}
+//
+//	private void sendMailTest() {
+//		List<String> emails = Arrays.asList("91178ce32b-0d1201@inbox.mailtrap.io");
+//		emailService.sendEmails("Testando serviço de e-mails.", emails);
+//		System.out.println("Email enviado!");
+//	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LibrayApiApplication.class, args);

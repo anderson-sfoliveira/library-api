ffce496482171243565962c0f8081b8bd865aea0
Anderson Oliveira <anderson.sfoliveira@gmail.com>
2022-09-26 17:03:03 -0300

    organization

--- src/main/java/br/com/brlsistemas/libraryapi/api/security/CorsFilter.java
+++ src/main/java/br/com/brlsistemas/libraryapi/api/security/CorsFilter.java
@@ -25,8 +25,8 @@ public class CorsFilter implements Filter {
 
 //	private final String[] originPermitida = {"http://localhost:9090"};
 //	private final String[] originPermitida = {"http://localhost:9090", "https://library-api-mycloud.herokuapp.com/"};
-//	private final String[] originPermitida = {"https://library-api-mycloud.herokuapp.com"};
-	private final String[] originPermitida = {"https://library-api-mycloud.herokuapp.com/actuator"};
+	private final String[] originPermitida = {"https://library-api-mycloud.herokuapp.com"};
+//	private final String[] originPermitida = {"https://library-api-mycloud.herokuapp.com/actuator"};
 
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)

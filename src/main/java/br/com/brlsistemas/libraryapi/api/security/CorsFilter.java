4603ec65adb437ce5a2d4c6759e21ba729a62499
Anderson Oliveira <anderson.sfoliveira@gmail.com>
2022-09-26 17:01:26 -0300

    organization

--- src/main/java/br/com/brlsistemas/libraryapi/api/security/CorsFilter.java
+++ src/main/java/br/com/brlsistemas/libraryapi/api/security/CorsFilter.java
@@ -23,10 +23,10 @@ public class CorsFilter implements Filter {
 	 * verificar se a origem tem permissão para acessar a API.
 	 */
 
-	private  String[] originPermitida = {"http://localhost:9090"};
+//	private final String[] originPermitida = {"http://localhost:9090"};
 //	private final String[] originPermitida = {"http://localhost:9090", "https://library-api-mycloud.herokuapp.com/"};
-//	private final String[] originPermitida = {"https://library-api-mycloud.herokuapp.com/actuator"};
-//	private final String[] originPermitida = {"https://library-api-mycloud.herokuapp.com/actuator1"};
+//	private final String[] originPermitida = {"https://library-api-mycloud.herokuapp.com"};
+	private final String[] originPermitida = {"https://library-api-mycloud.herokuapp.com/actuator"};
 
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)

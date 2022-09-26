6cebee95b231531a4170de3c9651814bbdd669be
Anderson Oliveira <anderson.sfoliveira@gmail.com>
2022-09-26 15:20:58 -0300

    organization

--- src/main/java/br/com/brlsistemas/libraryapi/api/security/CorsFilter.java
+++ src/main/java/br/com/brlsistemas/libraryapi/api/security/CorsFilter.java
@@ -11,48 +11,48 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
-
-@Component
-@Order(Ordered.HIGHEST_PRECEDENCE)
-public class CorsFilter implements Filter {
-
-	/**
-	 * Essa classe é responsável por controlar o CORS.
-	 * 
-	 * O Spring irá interceptar as requisições com o método OPTIONS para 
-	 * verificar se a origem tem permissão para acessar a API.
-	 */
-
-	private final String[] originPermitida = {"library-api-mycloud.herokuapp.com"};
-	
-	@Override
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-			throws IOException, ServletException {
-		
-		HttpServletRequest req = (HttpServletRequest) request;
-		HttpServletResponse resp = (HttpServletResponse) response;
-
-        Set<String> allowedOrigins = new HashSet<String>(Arrays.asList (originPermitida));
-        
-        String originHeader = req.getHeader("Origin");
-        
-        if (allowedOrigins.contains(originHeader)) {
-        	resp.setHeader("Access-Control-Allow-Origin", originHeader);
-        } else {
-        	allowedOrigins.forEach(p -> resp.setHeader("Access-Control-Allow-Origin", p));
-        }
-        
-		resp.setHeader("Access-Control-Allow-Credentials", "true");
-		
-		if ("OPTIONS".equals(req.getMethod()) && allowedOrigins.contains(originHeader)) {
-			resp.setHeader("Access-Control-Allow-Methods", "POST, DELETE, GET, PUT, PATCH, OPTIONS");
-			resp.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept");
-			resp.setHeader("Access-Control-Max-Age", "10"); // tempo do pre-flight para fazer a requisição OPTIONS novamente = 10 segundos
-			
-			resp.setStatus(HttpServletResponse.SC_OK);
-		} else {
-			chain.doFilter(req, resp);
-		}
-	}
-
-}
+public class CorsFilter{}
+//@Component
+//@Order(Ordered.HIGHEST_PRECEDENCE)
+//public class CorsFilter implements Filter {
+//
+//	/**
+//	 * Essa classe é responsável por controlar o CORS.
+//	 *
+//	 * O Spring irá interceptar as requisições com o método OPTIONS para
+//	 * verificar se a origem tem permissão para acessar a API.
+//	 */
+//
+//	private final String[] originPermitida = {"library-api-mycloud.herokuapp.com"};
+//
+//	@Override
+//	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+//			throws IOException, ServletException {
+//
+//		HttpServletRequest req = (HttpServletRequest) request;
+//		HttpServletResponse resp = (HttpServletResponse) response;
+//
+//        Set<String> allowedOrigins = new HashSet<String>(Arrays.asList (originPermitida));
+//
+//        String originHeader = req.getHeader("Origin");
+//
+//        if (allowedOrigins.contains(originHeader)) {
+//        	resp.setHeader("Access-Control-Allow-Origin", originHeader);
+//        } else {
+//        	allowedOrigins.forEach(p -> resp.setHeader("Access-Control-Allow-Origin", p));
+//        }
+//
+//		resp.setHeader("Access-Control-Allow-Credentials", "true");
+//
+//		if ("OPTIONS".equals(req.getMethod()) && allowedOrigins.contains(originHeader)) {
+//			resp.setHeader("Access-Control-Allow-Methods", "POST, DELETE, GET, PUT, PATCH, OPTIONS");
+//			resp.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept");
+//			resp.setHeader("Access-Control-Max-Age", "10"); // tempo do pre-flight para fazer a requisição OPTIONS novamente = 10 segundos
+//
+//			resp.setStatus(HttpServletResponse.SC_OK);
+//		} else {
+//			chain.doFilter(req, resp);
+//		}
+//	}
+//
+//}
